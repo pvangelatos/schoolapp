9b478c1d2ffe28a277c22859e1cf3eada7c42ca2
pvangelatos <p.vangelatos23@hotmail.com>
2026-02-05 12:21:41 +0200

    feat: add features for teacher's insert

--- src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
+++ src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
@@ -3,11 +3,16 @@ package gr.aueb.cf.schoolapp.controller;
 
 import gr.aueb.cf.schoolapp.dto.RegionReadOnlyDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -28,5 +33,27 @@ public class TeacherController {
                 new RegionReadOnlyDTO(1L, "Αθήνα"),
                 new RegionReadOnlyDTO(2L, "Βόλος"),
                 new RegionReadOnlyDTO(3L, "Θεσσαλονίκη"));
+    }
+
+    @PostMapping("/insert")
+    public String teacherInsert(@Valid @ModelAttribute("teacherInsertDTO") TeacherInsertDTO teacherInsertDTO,
+                                BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            // model.addAttribute("regionsReadOnlyDTO", regions());
+            return "teacher-insert";
+        }
+
+        // save teacher to DB
+        TeacherReadOnlyDTO teacherReadOnlyDTO = new TeacherReadOnlyDTO("acfd-1234", "Παναγιώτης", "Βαγγελάτος");
+        // model.addAttribute("teacherReadOnlyDTO", teacherReadOnlyDTO);
+
+        // PRG - Post - Redirect - Get
+        redirectAttributes.addFlashAttribute("teacherReadOnlyDTO", teacherReadOnlyDTO);
+        return "redirect:/teachers/success";
+    }
+
+    @GetMapping("/success")
+    public String teacherSuccess(Model model) {
+        return "teacher-success";
     }
 }
