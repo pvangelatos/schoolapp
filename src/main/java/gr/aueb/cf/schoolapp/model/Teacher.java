0f5da7395811f87cd382d116f6a27b28c0a7c4ad
pvangelatos <p.vangelatos23@hotmail.com>
2026-02-11 20:25:36 +0200

    fix: uncomment Teacher and abstractEntity

--- src/main/java/gr/aueb/cf/schoolapp/model/Teacher.java
+++ src/main/java/gr/aueb/cf/schoolapp/model/Teacher.java
@@ -1,19 +1,36 @@
 package gr.aueb.cf.schoolapp.model;
 
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-//@Entity
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Getter
-//@Setter
-//@Table(name = "teachers")
-//public class Teacher extends AbstractEntity {
-//
-//
-//}
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "teachers")
+public class Teacher extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(unique = true, nullable = false, updatable = false)
+    private UUID uuid;
+
+    @Column(unique = true)
+    private String vat;
+
+    private String firstname;
+    private String lastname;
+
+    @PrePersist
+    public void initializeUUID() {
+        this.uuid = UUID.randomUUID();
+    }
+}
