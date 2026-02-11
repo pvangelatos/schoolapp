83a58bcb4b5e69dc959175e57350b56199f7e652
pvangelatos <p.vangelatos23@hotmail.com>
2026-02-11 20:25:54 +0200

    fix: SchoolappApplication

--- src/main/java/gr/aueb/cf/schoolapp/SchoolappApplication.java
+++ src/main/java/gr/aueb/cf/schoolapp/SchoolappApplication.java
@@ -2,10 +2,10 @@ package gr.aueb.cf.schoolapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-//@EnableJpaAuditing
+@EnableJpaAuditing
 public class SchoolappApplication {
 
 	public static void main(String[] args) {
