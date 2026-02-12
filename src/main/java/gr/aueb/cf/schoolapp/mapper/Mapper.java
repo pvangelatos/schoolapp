1972830a6b197381daa8bb1d064b9513d3e1eb0a
pvangelatos <p.vangelatos23@hotmail.com>
2026-02-12 22:41:08 +0200

    chore: add regions logic

--- src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
@@ -1,8 +1,10 @@
 package gr.aueb.cf.schoolapp.mapper;
 
+import gr.aueb.cf.schoolapp.dto.RegionReadOnlyDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolapp.model.Teacher;
+import gr.aueb.cf.schoolapp.model.static_data.Region;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,5 +16,10 @@ public class Mapper {
 
     public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher) {
         return new TeacherReadOnlyDTO(teacher.getUuid().toString(), teacher.getFirstname(), teacher.getLastname());
+    }
+
+    public RegionReadOnlyDTO mapToRegionReadOnlyDTO(Region region) {
+        return new RegionReadOnlyDTO(region.getId(), region.getName());
+
     }
 }
