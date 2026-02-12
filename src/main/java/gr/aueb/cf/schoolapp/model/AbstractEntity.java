f7eb868532b49b06121a21a48f335c02454f1e2b
pvangelatos <p.vangelatos23@hotmail.com>
2026-02-13 00:00:59 +0200

    fix: add @LastModifiedDate at private instant updatedAt

--- src/main/java/gr/aueb/cf/schoolapp/model/AbstractEntity.java
+++ src/main/java/gr/aueb/cf/schoolapp/model/AbstractEntity.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
@@ -24,6 +25,7 @@ public abstract class AbstractEntity {
     @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "DATETIME")
     private Instant createdAt;      // UTC
 
+    @LastModifiedDate
     @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME")
     private Instant updatedAt;
 }
