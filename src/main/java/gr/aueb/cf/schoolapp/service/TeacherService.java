917f1bbba7a7dccdeb103c8d35bb548c654e6fe0
pvangelatos <p.vangelatos23@hotmail.com>
2026-02-14 22:59:03 +0200

    chore: add new service isTeacherExists

--- src/main/java/gr/aueb/cf/schoolapp/service/TeacherService.java
+++ src/main/java/gr/aueb/cf/schoolapp/service/TeacherService.java
@@ -55,4 +55,8 @@ public class TeacherService implements ITeacherService{
             throw e;
         }
     }
+
+    public boolean isTeacherExists(String vat) {
+        return teacherRepository.findByVat(vat).isPresent();
+    }
 }
